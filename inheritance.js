b27c30e4d7f607c4e6a105d85444314196e621ce
Daniel Boone <DanielBoone@Daniels-MacBook-Pro.local>
2015-09-18 13:56:53 -0500

    PUSH 3

--- inheritance.js
+++ inheritance.js
@@ -6,32 +6,63 @@ $(document).ready(function() {
     
     $('#btnAddCopCar').click(function() {
         var div = $('<div class="CopCar"></div>').attr('id', 'v'+ vid);
-        $(document.body).append(div);
+        $('#carCrash').append(div);
         var v = new CopCar('#v'+vid);
+        function Vehicledrop() {
+            var x = Math.floor((Math.random() * 1200) +1);
+            var y = Math.floor((Math.random() * 600) + 1);
+            $('#v'+vid).css("top",y).css("left",x);
+            }
+        Vehicledrop();
         v.move();
         vid++;
     });
     
     $('#btnAddCar').click(function() {
         var div = $('<div class="Car"></div>').attr('id', 'v'+ vid);
-        $(document.body).append(div);
+        $('#carCrash').append(div);
         var c = new Car('#v'+vid);
+        function Vehicledrop() {
+            var x = Math.floor((Math.random() * 1200) + 1);
+            var y = Math.floor((Math.random() * 600) + 1);
+            $('#v'+vid).css("top",y).css("left",x);
+            }
+        Vehicledrop();
         c.move();
         vid++;
     });
     
+    // $('#btnReverse').click(function() {
+    //     var div = $('<div class="Reverse"></div>').attr('id', 'v'+ vid);
+    //     $('#carCrash').append(div);
+    //     c.move();
+    //     vid++;
+    // });
+    
     $('#btnAddMotorcycle').click(function() {
         var div = $('<div class="Motorcycle"></div>').attr('id', 'v'+ vid);
-        $(document.body).append(div);
+        $('#carCrash').append(div);
         var m = new Motorcycle('#v'+vid);
+        function Vehicledrop() {
+            var x = Math.floor((Math.random() * 1200) + 1);
+            var y = Math.floor((Math.random() * 600) + 1);
+            $('#v'+vid).css("top",y).css("left",x);
+            }
+        Vehicledrop();
         m.move();
         vid++;
     });
     
     $('#btnAddTank').click(function() {
         var div = $('<div class="Tank"></div>').attr('id', 'v'+ vid);
-        $(document.body).append(div);
+        $('#carCrash').append(div);
         var t = new Tank('#v'+vid);
+        function Vehicledrop() {
+            var x = Math.floor((Math.random() * 1200) + 1);
+            var y = Math.floor((Math.random() * 600) + 1);
+            $('#v'+vid).css("top",y).css("left",x);
+            }
+        Vehicledrop();
         t.move();
         vid++;
     });
@@ -43,7 +74,7 @@ $(document).ready(function() {
     }
     Vehicle.prototype.moveRight = function() {
         $(this.divid).animate({
-            left: width}, 
+            left: width -100}, 
             { duration: this.speed, queue: false,
             complete: this.moveLeft.bind(this)}
         );
@@ -59,7 +90,7 @@ $(document).ready(function() {
     Vehicle.prototype.moveDown = function() {
         console.log(this.divid);
         $(this.divid).animate({
-            top: height}, 
+            top: height -100}, 
             { duration: this.speed, queue: false, 
             complete: this.moveUp.bind(this)}
         );
@@ -74,12 +105,22 @@ $(document).ready(function() {
             }
         );
     }
+    // Vehicle.prototype.moveReverse = function() {
+    //     console.log(this.divid);
+    //     $(this.divid).animate({}, { 
+    //             queue: false, 
+    //         }
+    //     );
+    // }
     Vehicle.prototype.move = function (direction) {
         this.moveRight();
         this.moveDown();
     }
         
-    Vehicle.prototype.remove = function () {}
+    // Vehicle.prototype.remove = function () {
+    //     if (e.which === 1) {
+    //     $('.tolerance').removeClass('Vehicle');
+    //  }
 
     function Car(vid) {
         this.tolerance = 2;
